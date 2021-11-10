ca93123c876d5e75644014d26b30ff6f0e20cd66
Babis Chalios <mail@bchalios.io>
2021-11-10 17:02:52 +0100

    Restrict function handlers to `FnOnce` + `Clone`
    This way, we give maximum flexibility to the consumer of the runtime and
    we make sure to clone the handler internally when needed.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- template/rust/openfaas-runtime/src/lib.rs
+++ template/rust/openfaas-runtime/src/lib.rs
@@ -14,34 +14,38 @@ extern crate serde_json;
 
 pub async fn run<Req, Resp, F>(handler: F)
 where
-    F: Fn(Req) -> Resp + Copy + Send + Sync + 'static,
+    F: FnOnce(Req) -> Resp + Clone + Send + Sync + 'static,
     Req: for<'de> Deserialize<'de> + Send,
     Resp: Serialize,
 {
     let make_service = make_service_fn(move |conn: &AddrStream| {
         let client_addr = conn.remote_addr();
 
-        let service = service_fn(move |req: Request<Body>| async move {
-            debug!("New request from {}", client_addr);
-            let body = req.into_body();
-            let body = hyper::body::to_bytes(body).await;
-            if let Err(_) = body {
-                error!("Could not parse body");
-                return Err("Runtime error");
-            }
+        let handler = handler.clone();
+        let service = service_fn(move |req: Request<Body>| {
+            let handler = handler.clone();
+            async move {
+                debug!("New request from {}", client_addr);
+                let body = req.into_body();
+                let body = hyper::body::to_bytes(body).await;
+                if let Err(_) = body {
+                    error!("Could not parse body");
+                    return Err("Runtime error");
+                }
 
-            let body = serde_json::from_slice(&body.unwrap());
-            if let Err(err) = body {
-                error!("Could not de-serialize request: {}", err);
-                return Ok(Response::new(Body::from("Runtime error")));
-            }
-
-            let resp = handler(body.unwrap());
-            match serde_json::to_vec(&resp) {
-                Ok(resp) => Ok(Response::new(Body::from(resp))),
-                Err(_) => {
-                    error!("Could not serialize response");
+                let body = serde_json::from_slice(&body.unwrap());
+                if let Err(err) = body {
+                    error!("Could not de-serialize request: {}", err);
                     return Ok(Response::new(Body::from("Runtime error")));
+                }
+
+                let resp = handler(body.unwrap());
+                match serde_json::to_vec(&resp) {
+                    Ok(resp) => Ok(Response::new(Body::from(resp))),
+                    Err(_) => {
+                        error!("Could not serialize response");
+                        return Ok(Response::new(Body::from("Runtime error")));
+                    }
                 }
             }
         });
