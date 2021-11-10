4d165ca747450e84c44b610a14b3b35e0847618c
Babis Chalios <mail@bchalios.io>
2021-11-10 11:32:17 +0100

    Add a function template
    Show how you can share state that has been created/initialized once
    across function invocations.
    
    Signed-off-by: Babis Chalios <mail@bchalios.io>

--- template/rust/function/src/main.rs
+++ template/rust/function/src/main.rs
@@ -1,10 +1,42 @@
-use openfaas_runtime::{Error, Request, Response};
-
 extern crate openfaas_runtime;
 
-async fn handle() -> Result<Response<Body>, Error> {}
+use log::debug;
+use std::sync::Arc;
+
+#[derive(Clone)]
+struct Greeter {
+    greet: &'static str,
+}
+
+impl Greeter {
+    fn new(greet: &'static str) -> Self {
+        Self { greet }
+    }
+
+    fn greet(&self, person: &str) -> String {
+        format!("{} {}", self.greet, person)
+    }
+}
+
+fn handler(req: serde_json::Value, greeter: Arc<Greeter>) -> String {
+    debug!("Received request: {}", req);
+
+    let response = if req["name"].is_string() {
+        greeter.greet(req["name"].as_str().unwrap())
+    } else {
+        "Doon't know the guy".to_string()
+    };
+    debug!("Responding: {}", response);
+
+    response
+}
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let greeter = Arc::new(Greeter::new("Hello"));
+    let handler = move |req: serde_json::Value| handler(req, greeter.clone());
+
+    openfaas_runtime::run(handler).await;
 }
